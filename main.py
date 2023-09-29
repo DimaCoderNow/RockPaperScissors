91f4c1b3e7b4fd473d7234d5db779650042ce67c
Dmitry Zamarekhin <dim.zam1986@gmail.com>
2023-09-29 22:25:06 +0200

    add menu

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import random
+
+
 hands = ["камень", "ножницы", "бумага"]
 win_hands = {"камень": "ножницы",
              "ножницы": "бумага",
              "бумага": "камень",
              }
+
+while True:
+    bot_hand = random.choice(hands)
+    # Меню
+    print("1 - камень")
+    print("2 - ножницы")
+    print("3 - бумага")
+    user_hand = input("Введите номер: ")
